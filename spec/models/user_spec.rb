3094850e1d51ce3f6b0fbc73e7e9b290c5f4c030
Chris Wright <chrswrght@gmail.com>
2012-07-10 20:53:47 +0200

    check in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -70,6 +70,17 @@ describe User do
   	end
   	
   	it { should_not be_valid }
+  end
+
+  describe "email addr with mixed case" do
+  	let(:mixed_case_email) { "fOo@ExaMplE.CoM" } 
+
+  	it "should be saved as all lower case" do
+  		@user.email = mixed_case_email
+  		@user.save
+  		@user.reload.email.should == mixed_case_email.downcase
+  	end
+  	
   end	
 
   describe "when password is not present" do
@@ -93,7 +104,7 @@ describe User do
   end	
 
   describe "return value of authenticate method" do
-  	before { "user.save" }
+  	before { @user.save }
   	let (:found_user) { User.find_by_email(@user.email) }
 
   	describe "with valid password" do
